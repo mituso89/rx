bed9a9081a4a551258262b27832ba0f961a07236
mituso <nguyenhuutuan1306@gmail.com>
2020-04-01 22:27:40 +0700

    create jwt

--- flight/routes.py
+++ flight/routes.py
@@ -1,6 +1,11 @@
+from flight.config import Config
 import os
 import secrets
+
 import flight.controllers.flightctl as search
+import jwt
+import datetime
+from flight.config import Config
 from flask import (render_template, url_for, flash, redirect, request, abort,
                    Blueprint, request, jsonify)
 
@@ -10,5 +15,16 @@ flight = Blueprint('flight', __name__)
 
 @flight.route('/get/flights', methods=['GET', 'POST'])
 def index():
-    
+
     return search.flightctl(request.get_json())
+
+
+@flight.route('/login', methods=['POST'])
+def login():
+    token = jwt.encode({
+        'email': request.get_json()['email'],
+        'password': request.get_json()['password'],
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60)
+    }, Config.SECRET_TOKEN)
+
+    return jsonify({'token': token.decode('utf-8')})
